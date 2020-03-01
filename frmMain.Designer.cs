d7ee2d2b343bb8e6e585b5f5d6fc7fd87c7335a8
Jathurshan Theivikaran <theivikaran.jathurshan@gmail.com>
2020-02-29 23:02:08 -0500

    Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace MazeWin
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileSolve = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileOpen,
+            this.mnuFileSolve,
+            this.toolStripMenuItem1,
+            this.mnuFileExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "File";
+            // 
+            // mnuFileOpen
+            // 
+            this.mnuFileOpen.Name = "mnuFileOpen";
+            this.mnuFileOpen.Size = new System.Drawing.Size(103, 22);
+            this.mnuFileOpen.Text = "Open";
+            this.mnuFileOpen.Click += new System.EventHandler(this.mnuFileOpen_Click);
+            // 
+            // mnuFileSolve
+            // 
+            this.mnuFileSolve.Name = "mnuFileSolve";
+            this.mnuFileSolve.Size = new System.Drawing.Size(103, 22);
+            this.mnuFileSolve.Text = "Solve";
+            this.mnuFileSolve.Click += new System.EventHandler(this.mnuFileSolve_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // mnuFileExit
+            // 
+            this.mnuFileExit.Name = "mnuFileExit";
+            this.mnuFileExit.Size = new System.Drawing.Size(152, 22);
+            this.mnuFileExit.Text = "Exit";
+            this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Maze";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileSolve;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using DrawGrid;
+using System.IO;
+
+/* Jathurshan Theivikaran
+ * December 2019
+ * this program solves a maze that the user inputs in a specific format
+ */
+
+//sample mazes are in the folder of this solution
+namespace MazeWin
+{
+    public partial class frmMain : Form
+    {
+        //private class variables
+        char[,] Maze;
+        int rows;
+        int cols;
+        Grid grid;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            //init any global variables
+            rows = 0;
+            cols = 0;
+        }
+
+        private void mnuFileOpen_Click(object sender, EventArgs e)
+        {
+            //get file from Open Dialog box
+            OpenFileDialog fd = new OpenFileDialog();
+
+            //don't forget error checking
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                //load the file
+                StreamReader sr = new StreamReader(fd.OpenFile());
+
+                rows = int.Parse(sr.ReadLine());
+                cols = int.Parse(sr.ReadLine());
+                //initialize the Maze array
+                Maze = new char[rows, cols];
+                grid = new Grid(new Size(cols, rows), 20, new Point(20, 20));
+
+                //populate the Maze array
+                for (int r = 0; r < rows; r++)
+                {
+                    string line = sr.ReadLine();
+                    for (int c = 0; c < cols; c++)
+                    {
+                        Maze[r, c] = line[c];
+                    }
+                }
+
+                //configure grid so each cell is drawn properly
+                ConfigureGrid();
+
+                //resize form to grid height and width
+                this.Width = this.cols * grid.CellSize + 60;
+                this.Height = this.rows * grid.CellSize + 80;
+
+                //tell form to redraw
+                this.Refresh();
+            }
+        }//mnuFileOpen Click method
+
+        private void ConfigureGrid()
+        {
+            for (int r = 0; r < this.rows; r++)
+            {
+                for (int c = 0; c < this.cols; c++)
+                {
+                    //change colour of cell depending on what
+                    //is in it
+                    if (Maze[r, c] == 'S') grid.GetCell(r, c).BackColor = Color.Purple;
+                    else if (Maze[r, c] == 'E') grid.GetCell(r, c).BackColor = Color.Blue;
+                    else if (Maze[r, c] == '#') grid.GetCell(r, c).BackColor = Color.Gray;
+                    else if (Maze[r, c] == '.') grid.GetCell(r, c).BackColor = Color.White;
+                    else if (Maze[r, c] == '+') grid.GetCell(r, c).BackColor = Color.Green;
+                    else if (Maze[r, c] == 'X') grid.GetCell(r, c).BackColor = Color.Yellow;
+                }
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (grid != null)
+                grid.Draw(e.Graphics);
+        }
+
+        private bool SolveMaze(int r, int c)
+        {
+            // if out of bounds of maze, return false
+            if (r < 0 || r >= rows || c < 0 || c >= cols) return false;
+            // if curr maze location is E, return true
+            if (Maze[r, c] == 'E')
+            {
+                Maze[r, c] = '+';
+                return true;
+            }
+
+            // if you cannot move on the following square, return false
+            // you have been on, not a wall
+            if (Maze[r, c] != '.' && Maze[r, c] != 'S') return false;
+
+            Maze[r, c] = '+';
+            // if you can, set to +
+
+            // call solvemaze
+            // if solvemaze to the right (all directions), return true
+            if (SolveMaze(r + 1, c)) return true;
+            if (SolveMaze(r, c + 1)) return true;
+            if (SolveMaze(r - 1, c)) return true;
+            if (SolveMaze(r, c - 1)) return true;
+            // else return false and change to X
+
+            Maze[r, c] = 'X';
+            return false;
+        }
+
+        private void mnuFileSolve_Click(object sender, EventArgs e)
+        {
+            //solve maze
+            SolveMaze(0, 0);
+            ConfigureGrid();
+
+            this.Refresh();
+        }
+
+        private void mnuFileExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
